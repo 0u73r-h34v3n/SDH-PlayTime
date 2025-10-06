515073d7459180eac99bd88070af518266a32246
ynhhoJ <22500212+ynhhoJ@users.noreply.github.com>
2025-10-06 15:12:10 +0300

    ref(py_modules/dto_time): Fixed bug when entries for manual time correction wasn't converted correctly

--- py_modules/dto/time/apply_manual_time_correction.py
+++ py_modules/dto/time/apply_manual_time_correction.py
@@ -1,42 +1,44 @@
-from typing import TypedDict, Optional
-
-
-class Game(TypedDict):
-    id: str
-    name: str
-
-
-class ApplyManualTimeCorrectionList(TypedDict):
-    game: Game
-    time: float
+from py_modules.schemas.common import Game
+from py_modules.schemas.request import ApplyManualTimeCorrectionList
 
 
 class ApplyManualTimeCorrectionDTO:
     def __init__(self, **kwargs):
-        self.start_date = kwargs.get("start_date", None)
-        self.end_date = kwargs.get("end_date", None)
-        self.game_id = kwargs.get("game_id", None)
-
+        raw_entries = kwargs.get("entries", [])
+        self.entries: list[ApplyManualTimeCorrectionList] = []
+        self._load_entries(raw_entries)
         self.validate_required_fields()
 
-    def _validate_field(
-        self, field_name: str, field_value: Optional[str], custom_message: str
-    ):
-        if field_value is None:
-            raise ValueError(f'"{field_name}" {custom_message}')
+    def _load_entries(self, raw_entries):
+        for entry in raw_entries:
+            game_data = entry["game"]
+            game = Game(id=game_data["id"], name=game_data["name"])
+
+            self.entries.append(
+                ApplyManualTimeCorrectionList(game=game, time=entry["time"])
+            )
 
     def validate_required_fields(self):
-        fields = [
-            ("start_date", self.start_date, '"start_date" must be a valid date'),
-            ("end_date", self.end_date, '"end_date" must be a valid date'),
-        ]
-
-        for field_name, field_value, message in fields:
-            self._validate_field(field_name, field_value, message)
+        if not isinstance(self.entries, list):
+            raise ValueError('"entries" must be a list')
 
     def to_dict(self):
-        return self.__dict__
+        return [
+            {
+                "game": {
+                    "id": entry.game.id,
+                    "name": entry.game.name,
+                },
+                "time": entry.time,
+            }
+            for entry in self.entries
+        ]
 
     @classmethod
     def from_dict(cls, dict_obj):
-        return cls(**dict_obj)
+        if not isinstance(dict_obj, list):
+            raise ValueError("Input must be a list of game-time entries")
+        return cls(entries=dict_obj)
+
+    def __iter__(self):
+        return iter(self.entries)
