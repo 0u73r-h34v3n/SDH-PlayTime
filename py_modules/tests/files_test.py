170069e254b052a239684fa661a451c8d5013ef4
ynhhoJ <22500212+ynhhoJ@users.noreply.github.com>
2025-10-08 19:28:00 +0300

    test(py_modules/files): Improve support of multi-threading test by creating unique file name per test

--- py_modules/tests/files_test.py
+++ py_modules/tests/files_test.py
@@ -1,3 +1,4 @@
+import datetime
 import hashlib
 import os
 import unittest
@@ -7,35 +8,45 @@ with patch.dict("os.environ", {"DECKY_PLUGIN_RUNTIME_DIR": "./python/tests/data"
     from py_modules.files import Files
 
 CHUNK_SIZE = 16 * 1024 * 1024
+DATE_FORMAT = "%y%m%d_%H%M%S%f"
 
 
 class TestFiles(unittest.TestCase):
     files: Files = Files()
 
     # Helper method to create a temporary file for testing
-    def create_temp_file(self, content):
+    def create_temp_file(self, file_path_suffix: str, content: bytes):
         file_path = "temp_file"
-        with open(file_path, "wb") as f:
+        filename = "_".join([file_path, file_path_suffix])
+
+        with open(filename, "wb") as f:
             f.write(content)
-        return file_path
+
+        return filename
 
     def test_empty_file(self):
         """Test case for empty file"""
-        file_path = self.create_temp_file(b"")
+        file_path = self.create_temp_file(
+            datetime.datetime.now().strftime(DATE_FORMAT), b""
+        )
         result = self.files.get_file_sha256(file_path)
         self.assertEqual(result, hashlib.sha256().hexdigest())
         os.remove(file_path)
 
     def test_small_file_within_chunk_size(self):
         """Test case for file smaller than 2 * chunk_size"""
-        file_path = self.create_temp_file(b"hello")
+        file_path = self.create_temp_file(
+            datetime.datetime.now().strftime(DATE_FORMAT), b"hello"
+        )
         result = self.files.get_file_sha256(file_path)
         self.assertEqual(result, hashlib.sha256(b"hello").hexdigest())
         os.remove(file_path)
 
     def test_file_exactly_chunk_size(self):
         """Test case for file exactly the size of CHUNK_SIZE"""
-        file_path = self.create_temp_file(b"A" * CHUNK_SIZE)
+        file_path = self.create_temp_file(
+            datetime.datetime.now().strftime(DATE_FORMAT), b"A" * CHUNK_SIZE
+        )
         result = self.files.get_file_sha256(file_path)
         self.assertEqual(result, hashlib.sha256(b"A" * CHUNK_SIZE).hexdigest())
         os.remove(file_path)
@@ -43,7 +54,9 @@ class TestFiles(unittest.TestCase):
     def test_file_larger_than_two_chunks(self):
         """Test case for file larger than 2 * CHUNK_SIZE"""
         content = b"A" * (CHUNK_SIZE * 3)
-        file_path = self.create_temp_file(content)
+        file_path = self.create_temp_file(
+            datetime.datetime.now().strftime(DATE_FORMAT), content
+        )
 
         expected_sha256 = hashlib.sha256()
         expected_sha256.update(content[:CHUNK_SIZE])
@@ -56,7 +69,9 @@ class TestFiles(unittest.TestCase):
 
     def test_file_exactly_two_chunks(self):
         """Test case for file exactly 2 * CHUNK_SIZE"""
-        file_path = self.create_temp_file(b"A" * (CHUNK_SIZE * 2))
+        file_path = self.create_temp_file(
+            datetime.datetime.now().strftime(DATE_FORMAT), b"A" * (CHUNK_SIZE * 2)
+        )
 
         expected_sha256 = hashlib.sha256()
         expected_sha256.update(b"A" * CHUNK_SIZE)
@@ -69,14 +84,18 @@ class TestFiles(unittest.TestCase):
 
     def test_single_byte_file(self):
         """Test case for file with a single byte"""
-        file_path = self.create_temp_file(b"A")
+        file_path = self.create_temp_file(
+            datetime.datetime.now().strftime(DATE_FORMAT), b"A"
+        )
         result = self.files.get_file_sha256(file_path)
         self.assertEqual(result, hashlib.sha256(b"A").hexdigest())
         os.remove(file_path)
 
     def test_large_file(self):
         """Test case for large file to ensure performance"""
-        file_path = self.create_temp_file(b"A" * (CHUNK_SIZE * 20))
+        file_path = self.create_temp_file(
+            datetime.datetime.now().strftime(DATE_FORMAT), b"A" * (CHUNK_SIZE * 20)
+        )
 
         # Check if the function runs without errors and returns a valid hash
         try:
@@ -89,7 +108,10 @@ class TestFiles(unittest.TestCase):
 
     def test_file_with_special_characters(self):
         """Test case for file with special characters"""
-        file_path = self.create_temp_file(b"@!#$%^&*()_+[]{}|;:'\"<>,./?")
+        file_path = self.create_temp_file(
+            datetime.datetime.now().strftime(DATE_FORMAT),
+            b"@!#$%^&*()_+[]{}|;:'\"<>,./?",
+        )
         result = self.files.get_file_sha256(file_path)
         expected_sha256 = hashlib.sha256(b"@!#$%^&*()_+[]{}|;:'\"<>,./?").hexdigest()
         self.assertEqual(result, expected_sha256)
@@ -103,7 +125,9 @@ class TestFiles(unittest.TestCase):
         ]
 
         for content, expected_sha256 in test_cases:
-            file_path = self.create_temp_file(content)
+            file_path = self.create_temp_file(
+                datetime.datetime.now().strftime(DATE_FORMAT), content
+            )
             result = self.files.get_file_sha256(file_path)
             self.assertEqual(result, expected_sha256)
             os.remove(file_path)
